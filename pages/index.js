9d537975a6a5b09e04a26079c06e572dbe5b6be6
sami shaikh <samishaikh05687@gmail.com>
2024-01-09 01:25:27 +0530

    Create HomePage

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
 import { v4 as uuidv4 } from 'uuid';
 import { useRouter } from 'next/navigation';
+import Image from "next/image";
+import videocall from '/pages/videocall.png'
+import styles from '@/styles/home.module.css'
 
 export default function Home() {
   const router = useRouter()
@@ -9,13 +12,18 @@ export default function Home() {
     router.push(`/${roomId}`)
   }
   return (
-    <div>
-      <h1>Echo Meet</h1>
-      <div>
+    <div className={styles.homeContainer}>
+      <Image src={videocall}  
+                    alt="Call-icon"
+                    width={70}
+                    height={70}/>
+      <h1>EchoMeet</h1>
+      <span className={styles.separatorText}>Enter Your Code To Join The Conservation</span>
+      <div className={styles.enterRoom}>
         <input /> 
         <button>Join Room</button>
       </div>
-      <span>-----------OR-------------</span>
+      <span className={styles.separatorText}>------------ OR ------------</span>
       <button>Create New Room </button>
     </div>
 
